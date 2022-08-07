7eb3b20aa631a81fa555467069234483e22323a5
Vyacheslav Morozov <slava.mor.98.sm@gmail.com>
2022-08-07 18:21:01 +0600

    add readme.md and python script with data generation

---
+++ data_generation.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import random
+
+
+def random_values_list(start, end):
+    initial = random.randint(start, end)
+
+    list_ = []
+    for i in initial:
+        list_.append(i)
+
+    return list_
+
+
+
+
